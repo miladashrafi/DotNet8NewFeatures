877f85509e4ffed9084c7aeb349b55d9324c074d
Reza Kazemi <R.Kazemi@Alibaba.ir>
2023-11-25 13:59:21 +0330

    feat: add primary constructor sample

---
+++ SamplesForDotNet/Models/PrimaryConstructorModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet8NewFeatures.Models
+{
+    public class PrimaryConstructorModel(int number)
+    {
+        public void increment() => number++;
+    }
+
+    public class OldConstructorModel
+    {
+        private int _d41d8cd98f00b204e9800998ecf8427e_number;
+        public OldConstructorModel(int number)
+        {
+            _d41d8cd98f00b204e9800998ecf8427e_number = number;
+        }
+        public int increment() => _d41d8cd98f00b204e9800998ecf8427e_number++;
+    }
+
+    public class PrimaryConstructorModel2(int number) : PrimaryConstructorModel(number)
+    {
+        public PrimaryConstructorModel2() : this(12)
+        {
+
+        }
+    }
+}
