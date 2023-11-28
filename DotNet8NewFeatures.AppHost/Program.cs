cc3743e2444091944cfc3f87cbd3f094736e3de1
mi.ashrafi <mi.ashrafi@alibaba.ir>
2023-11-28 18:17:26 +0330

    add comments

--- DotNet8NewFeatures.AppHost/Program.cs
+++ DotNet8NewFeatures.AppHost/Program.cs
@@ -1,12 +1,19 @@
-using Microsoft.Extensions.Configuration;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
+
+// comment to use external resources as containers, uncomment below section
+#region Only Use Resources with my own connection strings
 var redis = builder.AddRedis("Redis");
 var sql = builder.AddSqlServerConnection("SqlServer");
+#endregion
 
+//comment to use your own external resources, uncomment upper section
+#region Automatically create a container for every resource, use it and then clear on application shutdown.
 //var sql = builder.AddSqlServerContainer("SqlServer", "P@ssw0rd")
 //    .AddDatabase("dbtest").WithVolumeMount("c:\\sql\\data", "/var/opt/mssql/data");
+
+//var redis = builder.AddRedisContainer("Redis"); 
+#endregion
 
 builder.AddProject<Projects.DotNet8NewFeatures>("dotnet8newfeatures")
     .WithReference(redis)
