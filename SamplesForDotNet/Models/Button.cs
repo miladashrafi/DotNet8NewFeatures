aac194c52a5ad173fe8ab567c0ee8616b823b7bf
mi.ashrafi <mi.ashrafi@alibaba.ir>
2023-12-02 10:04:59 +0330

    fix

--- SamplesForDotNet/Models/Button.cs
+++ SamplesForDotNet/Models/Button.cs
@@ -6,9 +6,9 @@ namespace DotNet8NewFeatures.Models
     [JsonConverter(typeof(JsonStringEnumConverter<Button>))]
     public enum Button : byte
     {
-        Red,
-        Green,
-        Blue,
-        Yellow
+        Red = 1,
+        Green = 2,
+        Blue = 3,
+        Yellow = 4
     }
 }
